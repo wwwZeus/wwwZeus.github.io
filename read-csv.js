cfb32833f78ea9e15f8f36f4f26043799b1d884f
wwwZeus <46651388+wwwZeus@users.noreply.github.com>
2019-01-23 16:44:35 +0300

    Update read-csv.js

--- read-csv.js
+++ read-csv.js
@@ -155,7 +155,7 @@ function errorHandler(evt) {
 function drawOutput(lines){
 	//Clear previous data
 	//alert ('Вызов процедур');
-	alert ('asf v1.644');
+	alert ('asf v1.645');
 	document.getElementById("output").innerHTML = "";
 	var table = document.createElement("table");
 	for (var i = 0; i < lines.length; i++) {
