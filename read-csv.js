bc65f5d0c3f92c904fcf925ff4c3fd96222259da
wwwZeus <46651388+wwwZeus@users.noreply.github.com>
2019-02-01 21:10:29 +0300

    Update read-csv.js

--- read-csv.js
+++ read-csv.js
@@ -241,5 +241,5 @@ function ver () {
     console.log ("ready 1!");
     $("#title").css("background-color", "#d0f0c0");	
     var Title=document.getElementById("title");
-    Title.textContent = "  v. 1.8528";
+    Title.textContent = " _v. 1.8528";
 }
